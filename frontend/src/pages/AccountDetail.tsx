a855a226e975c1fe156c997fac644d7ffa932b50
WrBug <iwrbug@gmail.com>
2025-12-03 23:08:37 +0800

    fix: 移除未使用的Checkbox导入
    - 修复AccountDetail.tsx中未使用的Checkbox导入导致的编译错误

--- frontend/src/pages/AccountDetail.tsx
+++ frontend/src/pages/AccountDetail.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import { Card, Descriptions, Button, Space, Tag, Spin, message, Typography, Divider, Modal, Form, Input, Checkbox, Alert } from 'antd'
+import { Card, Descriptions, Button, Space, Tag, Spin, message, Typography, Divider, Modal, Form, Input, Alert } from 'antd'
 import { ArrowLeftOutlined, ReloadOutlined, EditOutlined } from '@ant-design/icons'
 import { useAccountStore } from '../store/accountStore'
 import type { Account } from '../types'
